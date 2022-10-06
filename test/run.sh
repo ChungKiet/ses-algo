17a36b10956b3231360deecf8ffd102fb909a26e
ChungKiet <chtkkiet@gmail.com>
2022-10-06 23:12:15 +0700

    add-report

--- test/run.sh
+++ test/run.sh
@@ -1,80 +1,80 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-	echo "Usage: ./run.sh </path/to/program/>"
+	echo "Usage: ./autorun_15p.sh </path/to/program/>"
 	exit
 fi
 
 # copy from global config to 15 different config file
-cp sample.conf pro_0.conf
-echo -e "log:log/process_0_log.txt\nid:0" >> pro_0.conf
-cp sample.conf pro_1.conf
-echo -e "log:log/process_1_log.txt\nid:1" >> pro_1.conf
-cp sample.conf pro_2.conf
-echo -e "log:log/process_2_log.txt\nid:2" >> pro_2.conf
-cp sample.conf pro_3.conf
-echo -e "log:log/process_3_log.txt\nid:3" >> pro_3.conf
-cp sample.conf pro_4.conf
-echo -e "log:log/process_4_log.txt\nid:4" >> pro_4.conf
-cp sample.conf pro_5.conf
-echo -e "log:log/process_5_log.txt\nid:5" >> pro_5.conf
-cp sample.conf pro_6.conf
-echo -e "log:log/process_6_log.txt\nid:6" >> pro_6.conf
-cp sample.conf pro_7.conf
-echo -e "log:log/process_7_log.txt\nid:7" >> pro_7.conf
-cp sample.conf pro_8.conf
-echo -e "log:log/process_8_log.txt\nid:8" >> pro_8.conf
-cp sample.conf pro_9.conf
-echo -e "log:log/process_9_log.txt\nid:9" >> pro_9.conf
-cp sample.conf pro_10.conf
-echo -e "log:log/process_10_log.txt\nid:10" >> pro_10.conf
-cp sample.conf pro_11.conf
-echo -e "log:log/process_11_log.txt\nid:11" >> pro_11.conf
-cp sample.conf pro_12.conf
-echo -e "log:log/process_12_log.txt\nid:12" >> pro_12.conf
-cp sample.conf pro_13.conf
-echo -e "log:log/process_13_log.txt\nid:13" >> pro_13.conf
-cp sample.conf pro_14.conf
-echo -e "log:log/process_14_log.txt\nid:14" >> pro_14.conf
+# cp global_15p.conf 15p_0.conf
+# echo -e "log:logs_15p/process_0_log.txt\nid:0" >> 15p_0.conf
+# cp global_15p.conf 15p_1.conf
+# echo -e "log:logs_15p/process_1_log.txt\nid:1" >> 15p_1.conf
+# cp global_15p.conf 15p_2.conf
+# echo -e "log:logs_15p/process_2_log.txt\nid:2" >> 15p_2.conf
+# cp global_15p.conf 15p_3.conf
+# echo -e "log:logs_15p/process_3_log.txt\nid:3" >> 15p_3.conf
+# cp global_15p.conf 15p_4.conf
+# echo -e "log:logs_15p/process_4_log.txt\nid:4" >> 15p_4.conf
+# cp global_15p.conf 15p_5.conf
+# echo -e "log:logs_15p/process_5_log.txt\nid:5" >> 15p_5.conf
+# cp global_15p.conf 15p_6.conf
+# echo -e "log:logs_15p/process_6_log.txt\nid:6" >> 15p_6.conf
+# cp global_15p.conf 15p_7.conf
+# echo -e "log:logs_15p/process_7_log.txt\nid:7" >> 15p_7.conf
+# cp global_15p.conf 15p_8.conf
+# echo -e "log:logs_15p/process_8_log.txt\nid:8" >> 15p_8.conf
+# cp global_15p.conf 15p_9.conf
+# echo -e "log:logs_15p/process_9_log.txt\nid:9" >> 15p_9.conf
+# cp global_15p.conf 15p_10.conf
+# echo -e "log:logs_15p/process_10_log.txt\nid:10" >> 15p_10.conf
+# cp global_15p.conf 15p_11.conf
+# echo -e "log:logs_15p/process_11_log.txt\nid:11" >> 15p_11.conf
+# cp global_15p.conf 15p_12.conf
+# echo -e "log:logs_15p/process_12_log.txt\nid:12" >> 15p_12.conf
+# cp global_15p.conf 15p_13.conf
+# echo -e "log:logs_15p/process_13_log.txt\nid:13" >> 15p_13.conf
+# cp global_15p.conf 15p_14.conf
+# echo -e "log:logs_15p/process_14_log.txt\nid:14" >> 15p_14.conf
 echo "Created temporary config files.\n"
 
 # execute all processes in background
 echo "Excuting processes in background..."
-$1 > /dev/null pro_0.conf &
+$1 > /dev/null 15p_0.conf &
 pid0=$!
-$1 > /dev/null pro_1.conf &
+$1 > /dev/null 15p_1.conf &
 pid1=$!
-$1 > /dev/null pro_2.conf &
+$1 > /dev/null 15p_2.conf &
 pid2=$!
-$1 > /dev/null pro_3.conf &
+$1 > /dev/null 15p_3.conf &
 pid3=$!
-$1 > /dev/null pro_4.conf &
+$1 > /dev/null 15p_4.conf &
 pid4=$!
-$1 > /dev/null pro_5.conf &
+$1 > /dev/null 15p_5.conf &
 pid5=$!
-$1 > /dev/null pro_6.conf &
+$1 > /dev/null 15p_6.conf &
 pid6=$!
-$1 > /dev/null pro_7.conf &
+$1 > /dev/null 15p_7.conf &
 pid7=$!
-$1 > /dev/null pro_8.conf &
+$1 > /dev/null 15p_8.conf &
 pid8=$!
-$1 > /dev/null pro_9.conf &
+$1 > /dev/null 15p_9.conf &
 pid9=$!
-$1 > /dev/null pro_10.conf &
+$1 > /dev/null 15p_10.conf &
 pid10=$!
-$1 > /dev/null pro_11.conf &
+$1 > /dev/null 15p_11.conf &
 pid11=$!
-$1 > /dev/null pro_12.conf &
+$1 > /dev/null 15p_12.conf &
 pid12=$!
-$1 > /dev/null pro_13.conf &
+$1 > /dev/null 15p_13.conf &
 pid13=$!
-$1 > /dev/null pro_14.conf &
+$1 > /dev/null 15p_14.conf &
 pid14=$!
 echo "Executed all processes."
 
 # wait for all processes to change signal handler
-echo "Sleep for 2 seconds."
-sleep 2
+echo "Sleep for 15 seconds."
+sleep 15
 
 # notice processes to start
 echo "Sending signals to processes..."
@@ -115,19 +115,19 @@ wait $pid14
 echo "All processes finished."
 
 # remove all temporary config file
-# rm pro_0.conf
-# rm pro_1.conf
-# rm pro_2.conf
-# rm pro_3.conf
-# rm pro_4.conf
-# rm pro_5.conf
-# rm pro_6.conf
-# rm pro_7.conf
-# rm pro_8.conf
-# rm pro_9.conf
-# rm pro_10.conf
-# rm pro_11.conf
-# rm pro_12.conf
-# rm pro_13.conf
-# rm pro_14.conf
+# rm 15p_0.conf
+# rm 15p_1.conf
+# rm 15p_2.conf
+# rm 15p_3.conf
+# rm 15p_4.conf
+# rm 15p_5.conf
+# rm 15p_6.conf
+# rm 15p_7.conf
+# rm 15p_8.conf
+# rm 15p_9.conf
+# rm 15p_10.conf
+# rm 15p_11.conf
+# rm 15p_12.conf
+# rm 15p_13.conf
+# rm 15p_14.conf
 echo "Removed all temporary config files."
